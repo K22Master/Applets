ec9a648093eed0d6fc3bed3e2dc64df49e44e654
Fuzi <ws.web@foxmail.com>
2022-11-25 22:31:54 +0800

    自定义组件的事件调用

--- components/test/test.js
+++ components/test/test.js
@@ -11,13 +11,25 @@ Component({
    * 组件的初始数据
    */
   data: {
-
+    count:1
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-
+    countAdd(e){
+      console.log(e.target.dataset.num);
+      this.setData({
+        count:this.data.count+1,
+      });
+      this._showCount();
+    },
+    _showCount(){
+      wx.showToast({
+        title: 'count是'+this.data.count,
+        icon:"none"
+      })
+    }
   }
 })
