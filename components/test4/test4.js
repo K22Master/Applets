9aba2899bcb231d4a936534370e8cf09166bf300
Fuzi <ws.web@foxmail.com>
2022-11-27 21:14:52 +0800

    单个插槽和多个插槽

---
+++ components/test4/test4.js
@@ -0,0 +1,27 @@
+// components/test4/test4.js
+Component({
+  options:{
+    // 在组件定义时，使用此选项启用多slot支持
+    multipleSlots:true
+  },
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
