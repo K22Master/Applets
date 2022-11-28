16b562d7157dad7ffd6deec36b5f80423593f9bd
Fuzi <ws.web@foxmail.com>
2022-11-28 23:06:02 +0800

    Mobx实例的计算属性和actions

--- store/store.js
+++ store/store.js
@@ -1,8 +1,20 @@
 // 此js用来存放Store实例
 // 通过observable方法创建一个实例对象
-import {observable} from 'mobx-miniprogram' 
+import {action, observable} from 'mobx-miniprogram' 
 // 将store实例暴露出去
 export const store=observable({
+  // 数据字段
   numA:10,
   numB:20,
+  // 计算属性
+  get sum(){
+    return this.numA+this.numB;
+  },
+  // 通过action方法修改属性
+  updateNumA:action(function(step){
+    this.numA+=step;
+  }),
+  updateNumB:action(function(step){
+    this.numB+=step;
+  }),
 });
